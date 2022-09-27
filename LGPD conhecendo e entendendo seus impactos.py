55e86a5fa439c777970a86f3dfbdf349c76c94e6
Patricia Sousa <81525412+PatriciaSousas@users.noreply.github.com>
2022-09-27 12:44:29 -0300

    Add files via upload

---
+++ LGPD conhecendo e entendendo seus impactos.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Os pontos principais previstos nesta lei com ênfase no entendimento das mudanças e impactos nas relações digitais que ela trará para o Brasil a partir de 2021, quando entrará em vigência!
+
+# Utilizei as visões RMODP, Referenced Model Open Distributed Processing, ou ISO-10746, que é um padrão arquitetural voltado pra sistemas abertos e distribuídos com foco em objeto e que se aplica bem didaticamente para fins de compreensão das visões necessárias para criar uma Política de Privacidade mais adequada
+
+# In[ ]:
+
+
+
+
+
+# ### Case de Projeto será o desenvolvimento dessa Política de Privacidade do site de produtos e serviços de monitoramento de veículos baseado nessas visões
+
+# Direcionadores do Negócio - Business Drivers (BD):
+# 
+# - Receita com serviço em tempo real;
+# - Serviço com uso de GEOLOCALIZAÇÃO;
+# - Operação Satisfatória x Disponibilidade;
+# - Privacidade (LGPD). Requisitos Funcionais (RF). Requisitos Não Funcionais. Engenharia. Tecnologia.
+
+# Requisitos Funcionais, o cliente executará as seguintes funções:
+# 
+# - Cadastro via Web: nome, endereço, CPF, e-mail, telefones para contato;
+# - Cadastramento dos bens;
+# - Dados do pagamento – Boleto ou Cartão de Crédito;
+# - Informações ao cliente sobre frete, prazo de entrega, desistência e cancelamento.
+
+# ## Políticas de Privacidade com a  LGPD
+
+# Então a primeira questão é: os dados coletados estão de acordo com a política de privacidade declarada? É importante seguir exatamente o que define a LGPD, para evitar autuação e aplicação de sanções.
+
+#  - O nome
+#  - O RG 
+#  - O endereço físico
+#  - O endereço eletrônico
+#  - O CPF 
+#  - Os dados a respeito do veículo
+#  - A permissão para instalação dos cookies, porque com base nos cookies, será feito o monitoramento do comportamento do titular de dados, podendo acontecer o envio de e-mail marketing e outras promoções relacionadas aos produtos e serviços.
+
+# ## Relatório de Impacto de Proteção de Dados (RIPD)
+
+# - descrição dos tipos de dados coletados;
+# - metodologia utilizada para a coleta;
+# - metodologia utilizada para a garantia da segurança das informações;
+# - análise do controlador;
+# - medidas, salvaguardas e mecanismos de mitigação de risco;
+# - consulta aos stakeholders quanto aos riscos do processamento em análise;
+# -rastreamento das não-conformidades da LGPD;
+# - instrumentos internos de controle (políticas, processos e procedimentos voltados a proteção de dados e privacidade);
+# - consulta junto aos stakeholders externos;
+# - medidas de mitigação ou minimização destes riscos;
+# - detalhamento do processamento;
+# - análise do processamento;
+# - quais os controles;
+# - ameaças/vulnerabilidades;
+# - elaboração de relatório;
+# - envio ao DPO (Data Protection Officer);
+# - entendimento da organização e processos envolvidos (Contexto);
+# - processo de avaliação de riscos;
+# - gerenciamento de riscos.
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
